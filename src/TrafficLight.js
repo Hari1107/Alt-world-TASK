315e1b740c176f166b689033d403c8d048a3ff9f
HariHaran K <112464204+Hari1107@users.noreply.github.com>
2024-05-07 20:15:33 +0530

    Update TrafficLight.js

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -1,25 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-function Light({ backgroundColor }) {
-  return (
-    <div
-      aria-hidden={true}
-      className="traffic-light"
-      style={{ backgroundColor }}
-    />
-  );
-}
-
-export default function TrafficLight({
-  initialColor = 'green',
-  config,
-  layout = 'vertical',
-}) {
-  const [currentColor, setCurrentColor] =
-    useState(initialColor);
+export default function TrafficLight({ trafficStates }) {
+  const [currentColor, setCurrentColor] = useState("green");
 
   useEffect(() => {
-    const { duration, next } = config[currentColor];
+    const { duration, next } = trafficStates[currentColor];
 
     const timerId = setTimeout(() => {
       setCurrentColor(next);
@@ -28,27 +13,17 @@ export default function TrafficLight({
     return () => {
       clearTimeout(timerId);
     };
-  }, [config,currentColor]);
+  }, [currentColor, trafficStates]);
 
   return (
-    <div
-      aria-live="polite"
-      aria-label={`Current light: ${currentColor}`}
-      className={[
-        'traffic-light-container',
-        layout === 'vertical' &&
-          'traffic-light-container--vertical',
-      ]
-        .filter((cls) => !!cls)
-        .join(' ')}>
-      {Object.keys(config).map((color) => (
-        <Light
-          key={color}
-          backgroundColor={
-            color === currentColor
-              ? config[color].backgroundColor
-              : undefined
-          }
+    <div className="traffic-light-container">
+      {Object.keys(trafficStates).map((color) => (
+        <div
+          className="traffic-light"
+          style={{
+            backgroundColor:
+              color === currentColor && trafficStates[color].backgroundColor
+          }}
         />
       ))}
     </div>
